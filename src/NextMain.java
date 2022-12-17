b62b0653680f0c427c6d2211a339c2e850a09121
Maryor-z <88011448+Maryor-z@users.noreply.github.com>
2022-12-17 09:24:31 +0100

    Update NextMain.java

--- src/NextMain.java
+++ src/NextMain.java
@@ -1,2 +1,29 @@
-package PACKAGE_NAME;public class NextMain {
+public class NextMain {
+    public static void main(String[] args) {
+        Movie movie = Movie.getMovie("A", "Raw");
+        movie.watchMovie();
+
+        Adventure raw = (Adventure) Movie.getMovie("A", "Jaw");
+        raw.watchMovie();
+
+        Object comedy = Movie.getMovie("C", "airpplane");
+        Comedy comedyMovie = (Comedy) comedy;
+        comedyMovie.watchComedy();
+
+        var airplane = Movie.getMovie("C", "Airplane");
+            airplane.watchMovie();
+
+            var plane = new Comedy("Airplaneaa");
+            plane.watchComedy();
+
+            Object unknownObject = Movie.getMovie("S", "Airplane");
+            if (unknownObject.getClass().getSimpleName() == "Comedy"){
+                Comedy c = (Comedy) unknownObject;
+                c.watchComedy();
+            } else if (unknownObject instanceof Adventure) {
+                ((Adventure) unknownObject).watchAdventure();
+            } else if (unknownObject instanceof ScienceFiction syfy) {
+                syfy.watchScienceFiction();
+            }
+    }
 }
